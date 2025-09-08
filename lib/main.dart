57bcb38748c0033e33a5c2df0ed0fa053d74bfa1
TheSilverCoder <ldenriquez85@gmail.com>
2025-09-08 19:38:45 +0800

    basic UI and modals

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:expense_app/expenses.dart';
+import 'package:expense_app/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
     ),
   );
 }
